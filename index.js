452a297c1bfb13df0c7f4c344015706f0692804a
Deven Umrania <umraniadeven10@gmail.com>
2024-02-08 23:32:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("<h1> neoG Assignment 21 </h1>");
+});
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).json({ error: "Something went wrong" });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ error: "No route found!" });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
